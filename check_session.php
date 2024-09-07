94dc242f543487ae6db79d3bb8c387fc8e8f8457
Totilla <tsm.antonia@gmail.com>
2024-09-07 19:40:50 +0200

    comentarios añadidos

--- check_session.php
+++ check_session.php
@@ -1,4 +1,8 @@
 <?php
+//Sesión: Almacenar y persistir datos del usuario mientras que navega en un sitio web
+//hasta que cierra sesión o cierra el navegador
+//estos datos se guardan en el servidor, son transparentes para el cliente
+//hay que utilizar session_start() en todos los archivos dónde quiera utilizar $_SESSION
 session_start();
 
 function is_logged_in() {
